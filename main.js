59d296186e9e9857b1d030c3fbbad7326f9f6198
LiJohnson <598420668@163.com>
2022-12-09 21:53:59 +0800

    feat(chess): move method

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ class Chess {
   offset = 0;
   position = 0;
   state = FlightChess.CHESS_STATUS.HOME;
+  moveTimes = 0;
+  jumpTimes = 0;
   constructor(color, offset) {
     this.color = color;
     this.offset = offset;
@@ -19,9 +21,44 @@ class Chess {
     this.position = 0;
   }
   move(step) {
-   
+    if (this.state === FlightChess.CHESS_STATUS.AIRPORT) {
+      this.state = FlightChess.CHESS_STATUS.FLIGHTING;
+    }
+    assert(this.state === FlightChess.CHESS_STATUS.FLIGHTING, "can not flight");
+
+    this.moveTimes++;
+    this.position += step;
+    if (this.position > FlightChess.ENDED_POSITION) {
+      let over = this.position - FlightChess.ENDED_POSITION;
+      this.position = FlightChess.ENDED_POSITION - over;
+    }
+    if (this.position === FlightChess.ENDED_POSITION) {
+      this.state = FLIGHTING.CHESS_STATUS.ENDED;
+      return;
+    }
+    if (this.position >= FlightChess.TURN_END_POSITION) {
+      return;
+    }
+    //TODO check attack
+    if (this.position === FlightChess.FLY_POSITION) {
+      this.position += 12;
+      this.jumpTimes += 1;
+      //TODO check attack
+    }
+    //TODO check attack
+    if (this.position % 4 === 1) {
+      this.position += 4;
+      this.jumpTimes += 1;
+    }
+    //TODO check attack
+    if (this.position === FlightChess.FLY_POSITION) {
+      this.position += 12;
+      this.jumpTimes += 1;
+      //TODO check attack
+    }
+    //TODO check attack
   }
-  crash() {}
+  attack() {}
 }
 
 class Player {
@@ -48,6 +85,13 @@ class Player {
       FlightChess.OFFSET[index]
     );
   }
+
+  isWin() {
+    return [this.chessA, this.chessB, this.chessC, this.chessD]
+      .map((c) => c.state)
+      .every((state) => state === FlightChess.CHESS_STATUS.ENDED);
+  }
+
   move(chess, step) {}
 }
 
@@ -64,6 +108,7 @@ class FlightChess {
   static ENDED_POSITION = 56;
   static CLASH_POSITION = 53;
   static FIRST_POSITION_COLOR_INDEX = 3;
+  static FLY_POSITION = 17;
 
   constructor(playerNum) {}
 }
