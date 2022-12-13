ec86d43f992a8055507d07a88dbb20c86b4218c1
LiJohnson <598420668@163.com>
2022-12-13 22:11:08 +0800

    feat: demo version ko

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-assert = console.assert;
+assert = (exp, message) => {
+  if (exp) return
+  throw new Error(message)
+}
 
 class Chess {
   color = "";
@@ -10,9 +13,12 @@ class Chess {
   constructor(color, offset) {
     this.color = color;
     this.offset = offset;
+    this.position = -1
   }
 
   getGlobalPosition() {
+    if(this.position <1 )return -1
+    if(this.position > FlightChess.TURN_END_POSITION )return -2
     return (this.offset + this.position) % FlightChess.TOTAL_STEP;
   }
   startup(callback = () => { }) {
@@ -55,7 +61,7 @@ class Chess {
     callback(action);
 
     if (this.position === FlightChess.ENDED_POSITION) {
-      this.state = FLIGHTING.CHESS_STATUS.ENDED;
+      this.state = FlightChess.CHESS_STATUS.ENDED;
       callback({ chess: this, type: FlightChess.CHESS_ACTION.LAND });
       return;
     }
@@ -69,7 +75,7 @@ class Chess {
       if (checkAttak(this, FlightChess.CLASH_POSITION)) return;
     }
     if (checkAttak(this)) return;
-    if (this.position % 4 === 1) {
+    if (this.position % 4 === 2) {
       this.position += 4;
       this.jumpTimes += 1;
     }
@@ -84,7 +90,7 @@ class Chess {
   crash(callback) {
     assert(this.state === FlightChess.CHESS_STATUS.FLIGHTING, "is not flighting")
     this.state = FlightChess.CHESS_STATUS.HOME
-    this.position = 0
+    this.position = -1
     callback({ chess: this, type: FlightChess.CHESS_ACTION.CRASH });
   }
 }
@@ -151,11 +157,14 @@ class FlightChess {
   static ENDED_POSITION = 56;
   static CLASH_POSITION = 53;
   static FIRST_POSITION_COLOR_INDEX = 3;
-  static FLY_POSITION = 17;
-
+  static FLY_POSITION = 18;
+  static FLY_STEP = 12
+  static JUMP_STEP = 4
+  static AIRPORT_DICE_NUM = [6]
   players;
   recoads;
   round = {};
+
   constructor(playerNum) {
     if (playerNum === 2) {
       this.players = [0, 2].map((i) => new Player(i));
@@ -171,56 +180,93 @@ class FlightChess {
 
   async play() {
     while (true) {
-      this.players.forEach(async p => {
-        this.round.play = p;
-        let rollTimes = 1;
-        for (let i = 0; i < rollTimes; i++) {
-          let step = await new Promise(reslove => {
-            this.round.rollDice = num => reslove(num)
-          })
-          let chess = await new Promise(reslove => {
-            this.round.selectChess = chess => reslove(chess)
-          })
-          chess.move(step, this.addRecord, this.checkAttak)
-          if( p.isWin() ){
-            console.log("game over")
-            return 
-          }
-          if (step === 6) { rollTimes++ }
-          
-          if (rollTimes === 3) {
-            p.listChess().filter(c => c.state === FLIGHTING.CHESS_STATUS.FLIGHTING).forEach(c => c.crash(this.recoads))
+      for (let p of this.players) {
+        {
+          this.round.player = p;
+          let rollTimes = 1;
+          for (let i = 0; i < rollTimes; i++) {
+            this.round.action = `${p.color} to roll dice`
+            let step = await new Promise(reslove => {
+              this.round.rollDice = num => reslove(num)
+            })
+            this.addRecord({
+                player:p,
+                type: 'roll dice',
+                step:step
+            })
+            this.round.step = step
+            delete this.round.rollDice
+
+            let isStartupStepNum = FlightChess.AIRPORT_DICE_NUM.some(num => num === step)
+            let hasStartupChess = p.listChess().some(c => c.state === FlightChess.CHESS_STATUS.AIRPORT || c.state === FlightChess.CHESS_STATUS.FLIGHTING)
+            if (!isStartupStepNum && !hasStartupChess) {
+              continue
+            }
+            if (step === 6) { rollTimes++ }
+            if (rollTimes === 4) {
+              rollTimes = 0
+              p.listChess().filter(c => c.state === FlightChess.CHESS_STATUS.FLIGHTING).forEach(c => c.crash(c => this.addRecord(c)))
+              continue
+            }
+            this.round.action = `to select ${p.color} chess , and move step ${step}`
+            let chess = await new Promise(reslove => {
+              this.round.selectChess = chess => reslove(chess)
+            })
+            delete this.round.selectChess
+            if (chess.state === FlightChess.CHESS_STATUS.HOME && FlightChess.AIRPORT_DICE_NUM.find(num => num === this.round.step)) {
+              chess.startup(a => this.addRecord(a))
+            } else {
+              chess.move(step, a => this.addRecord(a), (c, p) => this.checkAttak(c, p))
+            }
+            if (p.isWin()) {
+              console.log("game over")
+              return
+            }
           }
         }
-      })
+      }
     }
   }
+  // s = [2, 6]
   rollDice() {
-    this.round.rollDice(Math.floor(Math.random() * 6 + 1))
+    assert(this.round && this.round.rollDice, "can not roll dice now")
+    let step = Math.floor(Math.random() * 6 + 1)
+    // let step = this.s.pop()
+    this.round.rollDice(step)
+    return step
   }
   selectChess(chess) {
-    if (chess.color === this.dice.player.color) {
-      this.round.selectChess(chess)
+    assert(this.round.player && this.round.selectChess, "cant not select chess now")
+    assert(chess.color === this.round.player.color, "select chess color error")
+    assert(chess.state !== FlightChess.CHESS_STATUS.ENDED, "chess is landed")
+    if (chess.state === FlightChess.CHESS_STATUS.HOME) {
+      assert(FlightChess.AIRPORT_DICE_NUM.some(num => this.round.step === num), "can not startup")
     }
+
+    this.round.selectChess(chess)
   }
   checkAttak(chess, position) {
-    if (position === FLIGHTING.CLASH_POSITION) {
+    if (position === FlightChess.CLASH_POSITION) {
       let index = (this.round.player.colorIndex + 2) % 4
-      this.players.filter(p => p.colorIndex === index).flatMap(p => p.listChess()).filter(c => c.position === FLIGHTING.CLASH_POSITION).forEach(c => {
-        c.crash(this.addRecord)
+      this.players.filter(p => p.colorIndex === index).flatMap(p => p.listChess()).filter(c => c.position === FlightChess.CLASH_POSITION).forEach(c => {
+        c.crash(a => this.addRecord(a))
       })
       return
     }
-    this.players.filter(p => p.color !== chess.color).flatMap(p => p.listChess()).forEach(beAttackedChess => {
-      if (chess.position === beAttackedChess.position) {
-        beAttackedChess.crash(this.addRecord)
-      }
-    })
+    this.players.filter(p => p.color !== chess.color).flatMap(p => p.listChess())
+      .filter(c => c.state === FlightChess.CHESS_STATUS.FLIGHTING)
+      .filter(c => c.position <= FlightChess.TURN_END_POSITION)
+      .forEach(beAttackedChess => {
+        if (chess.getGlobalPosition() === beAttackedChess.getGlobalPosition()) {
+          beAttackedChess.crash(c => this.addRecord(c))
+        }
+      })
   }
   isOver() {
     return this.players.find(p => p.isWin())
   }
-
-
-  addRecord(action) { this.recoads.push(action) }
+  addRecord(action) {
+    // recoads.push(action)
+    this.recoads.push(action)
+  }
 }
